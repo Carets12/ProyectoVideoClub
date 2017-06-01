42dccdc18d79999308180d6fbbe2b5e407f89f11
Daniel Sierra <Danielsierraraez@gmail.com>
2017-06-01 12:14:28 +0200

    BD V0.1
    Bases de Datos de mi proyecto...

---
+++ BD/alquila.sql
@@ -0,0 +1,8 @@
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('01/05/2016', '14/06/2016', '25323421', '34709833A');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('03/06/2016', '20/07/2016', '15321627', '14895644X');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('20/07/2016', '12/08/2016', '85321221', '90083336J');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('13/07/2016', '20/06/2017', '55326211', '68434553K');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('16/05/2016', '17/08/2016', '35323231', '10340989F');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('20/08/2016', '01/01/2017', '75121221', '82106710H');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('09/11/2016', '10/12/2016', '89321721', '98767890Z');
+insert into alquila (fecha_alquilada, fecha_devolucion, dniCliente, codPelicula) values ('01/12/2016', '12/12/2016', '62323251', '90456792M');
